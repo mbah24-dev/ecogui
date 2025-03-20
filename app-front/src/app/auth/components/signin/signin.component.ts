02004eb2bdd1159b19724bbabc5f3bd7f44876bf
BAH Mamad <mbah@student.42lyon.fr>
2025-03-20 14:15:44 +0100

    ✨ feat(signin): amélioration de la gestion des rôles utilisateurs 🔐

--- app-front/src/app/auth/components/signin/signin.component.ts
+++ app-front/src/app/auth/components/signin/signin.component.ts
@@ -1,37 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth.service';
 import { Router, RouterLink } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgIf, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-signin',
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, NgIf, ReactiveFormsModule, NgStyle],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.scss'
 })
 export class SigninComponent {
+  signinForm!: FormGroup;
+  submitted: boolean = false;
   componentTilte: string = "Bconnect Shop";
-  credentials: {email: string, password: string} = {email: '', password: ''};
   role: 'buyer' | 'seller' | 'admin' = 'buyer';
   errorMessage: string = '';
 
   constructor(
     private authService: AuthService,
-    private router: Router) {}
-  signin() {
-    console.log(this.credentials);
-    this.authService.signin(this.role, this.credentials).subscribe(
-      {
-        next: response => {
-          console.log("Connexion réussie", response);
-          this.router.navigate(['/dashboard']);
-        },
-        error: error => {
-          console.error('Erreur de connexion', error);
-          this.errorMessage = "Email ou Mot de passe incorrect";
-        }
-      }
-    );
+    private router: Router, private formsBuilder: FormBuilder) {
+      this.signinForm = this.formsBuilder.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]]
+      });
+    }
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.signinForm.value);
+    if (this.signinForm.valid) {
+      this.signin(this.role, this.signinForm.value);
+    }
   }
 
+  signin(role: 'buyer' | 'seller' | 'admin', data: {email: string, password: string}) {
+    this.authService.signin(role, data).subscribe({
+      next: (response) => {
+        localStorage.setItem('accessToken', response.accessToken);
+        localStorage.setItem('user', JSON.stringify(response.user));
+        this.router.navigate(['/dashboard']);
+      },
+      error: (error) => {
+        this.errorMessage = "Email ou Mot de passe incorrect";
+      }
+    });
+  }
 }
