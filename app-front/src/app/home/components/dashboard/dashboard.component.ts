b98f015136d9ef882f454335a600047253dc26af
BAH Mamad <mbah@student.42lyon.fr>
2025-03-20 14:16:34 +0100

    🛠️ refactor(dashboard): adaptation de l'affichage selon le rôle utilisateur 📊

--- app-front/src/app/home/components/dashboard/dashboard.component.ts
+++ app-front/src/app/home/components/dashboard/dashboard.component.ts
@@ -1,20 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../auth/auth.service';
 import { Router } from '@angular/router';
+import { User } from '../../../types/app.types';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   constructor(private authService: AuthService, private router: Router) {}
+  usersConnected?: User;
+  errorMessage: string = '';
+
+  ngOnInit(): void {
+      this.get_users_info();
+  }
 
   logout() {
     this.authService.logout().subscribe(() => {
         console.log("Déconnexion réussie");
+        this.usersConnected = undefined;
         this.router.navigate(['/signin']);
     })
+  }
+
+  get_users_info() {
+    this.authService.get_users_info().subscribe({
+      next: response => {
+        this.usersConnected = response.user;
+      },
+      error: error => {
+        console.error('Erreur lors de la récupération des infos utilisateur', error);
+        this.errorMessage = 'Erreur lors de la récupération de vos informations. Veuillez vous reconnecter.';
+      }
+    });
   }
 }
