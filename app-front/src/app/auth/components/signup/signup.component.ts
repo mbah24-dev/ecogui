108ca1f356f3052f33a038b89a9db78ca8b53a58
BAH Mamad <mbah@student.42lyon.fr>
2025-03-20 14:16:10 +0100

    🚀 feat(signup): optimisation du processus d'inscription en fonction des rôles 🎭

--- app-front/src/app/auth/components/signup/signup.component.ts
+++ app-front/src/app/auth/components/signup/signup.component.ts
@@ -1,37 +1,48 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../auth.service';
 import { Router, RouterLink } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { NgIf, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, NgIf, ReactiveFormsModule, NgStyle],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+    signupForm!: FormGroup;
+    submitted: boolean = false;
     componentTilte: string = "Bconnect Shop";
-    userData = {
-      email: '',
-      password: '',
-      name: ''
-    };
     role: 'buyer' | 'seller' = 'buyer';
     errorMessage: string = '';
 
-    constructor(private authService: AuthService, private router: Router) {}
-    signup() {
-      console.log(this.userData);
-      this.authService.signup(this.role, this.userData).subscribe(
+    constructor(private authService: AuthService, private router: Router, private formBuilder: FormBuilder) {
+      this.signupForm = this.formBuilder.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        name: ['', [Validators.required, Validators.minLength(2)]]
+      });
+    }
+
+    onSubmit() {
+      this.submitted = true;
+      if (this.signupForm.valid) {
+        this.signup(this.role, this.signupForm.value);
+      }
+    }
+
+    signup(role:'buyer' | 'seller', userData: {email: string, password: string, name: string}) {
+      this.authService.signup(role, userData).subscribe(
         {
           next: response => {
-            console.log("Inscription réussie", response);
+            localStorage.setItem('accessToken', response.accessToken);
+            localStorage.setItem('user', JSON.stringify(response.user));
             this.router.navigate(['/dashboard']);
           },
           error: error => {
-            console.log("Erreur d\'inscription", error);
-            this.errorMessage = 'Échec de l\'inscription. Veuillez réessayer.';
+            this.errorMessage = 'Inscription échouée. Vérifiez vos infos..';
           }
         }
       );
