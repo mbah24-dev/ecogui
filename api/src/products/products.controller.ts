cc2b093986633de706a27bb070771e2bc1f010b6
BAH Mamad <mbah@student.42lyon.fr>
2025-03-27 23:38:29 +0100

    ✨ Ajout du module Produits avec fonctionnalités complètes 🛍️
    📦 Création du module products : Gestion des produits dans l'application.
    
    Routes et fonctionnalités ajoutées :
    Ajout de produit
    
    �� POST /products/add-product : Permet à un vendeur ou un admin d'ajouter un nouveau produit.
    
    🔒 Protégé par JwtAuthGuard et AdminOrSellerGuard pour assurer que seul un utilisateur authentifié et avec les bons droits puisse créer un produit.
    
    Obtenir tous les produits disponibles
    
    📜 GET /products/available/all : Permet d'obtenir tous les produits qui sont actuellement disponibles à la vente.
    
    Obtenir tous les produits
    
    📜 GET /products/all : Permet d'obtenir tous les produits dans le système (accessible uniquement par les admins).
    
    Obtenir tous les produits en attente d'approbation
    
    📜 GET /products/pending/all : Permet aux admins d'afficher tous les produits qui attendent une validation avant d'être mis en ligne.
    
    Approuver un produit par l'admin
    
    🔄 PUT /products/admin/approved/product=:productId : Permet à un admin d'approuver un produit spécifique (en fonction de l'ID).
    
    Obtenir un produit par son ID
    
    🔎 GET /products/id=:productId : Permet d'afficher un produit spécifique en fonction de son ID.
    
    Obtenir les produits par catégorie
    
    📚 GET /products/category=:name : Permet d'afficher les produits d'une catégorie spécifique.
    
    Obtenir toutes les catégories
    
    🏷️ GET /products/all-category : Permet d'obtenir toutes les catégories de produits disponibles.
    
    Supprimer un produit
    
    🗑️ DELETE /products/id=:productId : Permet à un vendeur ou admin de supprimer un produit spécifique.
    
    Mettre à jour un produit
    
    🛠️ PUT /products/id=:productId : Permet à un vendeur ou admin de mettre à jour les informations d'un produit existant.
    
    Sécurisation et contrôle des rôles :
    🔐 Utilisation de guards pour vérifier l'authentification des utilisateurs (JwtAuthGuard) et leur rôle (admin ou vendeur).
    
    ✔️ Protection des routes critiques pour garantir que seul un utilisateur autorisé puisse effectuer des actions sur les produits (ajout, mise à jour, suppression).

---
+++ api/src/products/products.controller.ts
@@ -0,0 +1,116 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   products.controller.ts                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbah <mbah@student.42lyon.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/26 03:43:12 by mbah              #+#    #+#             */
+/*   Updated: 2025/03/27 17:58:54 by mbah             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+import { Body, Controller, Post, Get, Req, Res, UseGuards, Param, Delete, Put } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
+import { CreateProductDto } from 'src/dto/products-dto/create-product.dto';
+import { Request as RequestExpressSession, Response } from 'express';
+import { IsAdmin } from 'src/decorator/is-admin.decorator';
+import { IsSeller } from 'src/decorator/is-seller.decorator';
+import { UpdateProductDto } from 'src/dto/products-dto/update-product.dto';
+import { AdminOrSellerGuard } from 'src/guards/admin-or-seller.guard';
+import { AdminGuard } from 'src/guards/admin.guard';
+import { AdminOrBuyerGuard } from 'src/guards/admin-or-buyer.guard';
+
+@Controller('products')
+export class ProductsController {
+	constructor(private readonly productsService: ProductsService) {}
+
+	@UseGuards(JwtAuthGuard, AdminOrSellerGuard)
+	@IsSeller()
+	@Post('add-product')
+	async create_product(@Body() body: CreateProductDto, @Req() req: RequestExpressSession, @Res() res: Response) {
+		if (!req.session.user) {
+			return res.status(401).json({ message: 'Utilisateur non connecté' });
+		}
+		try {
+			const productCreated = await this.productsService.create_product(body, req.session.user.id);
+			return res.json( productCreated );
+		} catch (error) {
+			return res.status(400).json({ message: error.message || 'Erreur inconnue lors de l\'ajout du produit' });
+		}
+	}
+
+	@Get('available/all')
+	async get_all_available_products() {
+		return (await this.productsService.get_all_available_products());
+	}
+	
+	@UseGuards(JwtAuthGuard, AdminGuard)
+	@Get('all')
+	async get_all_products() {
+		return (await this.productsService.get_all_products());
+	}
+
+	@UseGuards(JwtAuthGuard, AdminGuard)
+	@Get('pending/all')
+	async get_all_pending_products_by_admin() {
+		return (await this.productsService.get_all_pending_products_by_admin());
+	}
+
+	@UseGuards(JwtAuthGuard, AdminGuard)
+	@Put('admin/approved/product=:productId')
+	async approved_seller_product_by_id(@Param('productId') p_id: string) {
+		return (await this.productsService.approved_seller_product_by_id(p_id));
+	}
+
+	@Get('id=:productId')
+	async get_product_by_id(@Param('productId') productId: string) {
+		return (await this.productsService.get_product_by_id(productId));
+	}
+
+	@Get('category=:name')
+	async get_products_by_category(@Param('name') category: string) {
+		return (await this.productsService.get_products_by_category(category));
+	}
+
+	@Get('all-category')
+	async get_all_category() {
+		return (await this.productsService.get_all_category());
+	}
+
+	@UseGuards(JwtAuthGuard, AdminOrSellerGuard)
+	@IsAdmin()
+	@IsSeller()
+	@Delete('id=:productId')
+	async delete_product(@Param('productId') productId: string, @Req() req: RequestExpressSession, @Res() res: Response) {
+		if (!req.session.user) {
+			return res.status(401).json({ message: 'Utilisateur non connecté' });
+		}
+		try {
+			const product_deleted = await this.productsService.delete_product(productId, req.session.user.id);
+			return (res.json( product_deleted ));
+		} catch (error) {
+			return (res.status(400).json({ message: error.message || 'Erreur inconnue lors de la suppression du produit' }));
+		}
+	}
+
+	@UseGuards(JwtAuthGuard, AdminOrSellerGuard)
+	@IsSeller()
+	@IsAdmin()
+	@Put('id=:productId')
+	async update_product(@Param('productId') productId: string,
+		@Req() req: RequestExpressSession, @Res() res: Response,
+		@Body() data: UpdateProductDto) {
+		if (!req.session.user) {
+			return res.status(401).json({ message: 'Utilisateur non connecté' });
+		}
+		try {
+			const product_updated = await this.productsService.update_product(productId, req.session.user.id, data);
+			return (res.json( product_updated ));
+		} catch (error) {
+			return (res.status(400).json({ message: error.message || 'Erreur inconnue lors de la mise a jour du produit' }));
+		}
+	}
+
+}
