bb8c01e99325dd404fa22cc8550e2cdec00f3ee3
BAH Mamad <mbah@student.42lyon.fr>
2025-05-04 12:56:43 +0200

    refactor: changement de la base de donnee

--- api/src/prisma/prisma.service.ts
+++ api/src/prisma/prisma.service.ts
@@ -6,7 +6,7 @@
 /*   By: mbah <mbah@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 22:18:35 by mbah              #+#    #+#             */
-/*   Updated: 2025/04/30 18:21:24 by mbah             ###   ########.fr       */
+/*   Updated: 2025/05/03 17:21:06 by mbah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,29 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
 
-  async onModuleInit() {
-    try {
-      await this.$connect();
-      console.log('✅ Connected to the database successfully!');
-      await this.pingDatabase();
-    } catch (error) {
-      console.error('❌ Failed to connect to the database:', error);
-      process.exit(1);
-    }
-  }
+	async onModuleInit() {
+		let retries = 10; // Augmenter les tentatives
+		const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+	  
+		while (retries > 0) {
+		  try {
+			await this.$connect();
+			await this.pingDatabase();
+			console.log('✅ Connected to the database successfully!');
+			return;
+		  } catch (error) {
+			retries--;
+			console.error(`❌ Failed to connect to the database. Retries left: ${retries}`, error);
+			if (retries === 0) {
+			  console.error("❌ All connection attempts failed. Exiting...");
+			  process.exit(1);  // Quitter l'application si toutes les tentatives échouent
+			}
+			await delay(5000); // Attendre 5 secondes avant la prochaine tentative
+		  }
+		}
+	}
+	  
+	  
 
   async onModuleDestroy() {
     await this.$disconnect();
